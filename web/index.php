3d7e6f3ebd1bef42adf5b32681545bdfce55d500
Vedha <vraje059@uottawa.ca>
2016-03-20 11:47:55 -0400

    added echo statement to print results

--- web/index.php
+++ web/index.php
@@ -11,7 +11,18 @@ $dbconn = pg_connect("host=web0.site.uottawa.ca port=15432 dbname=vraje059 user=
     or die('Could not connect: ' . pg_last_error());
 
 $query = "SELECT * FROM CUSTOMER";
-$result = pg_query($query);
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+// Printing results in HTML
+echo "<table>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
 
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
