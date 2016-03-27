60d3b12dac065cda59e65c3762813f3ba1e4dc2f
victordiec <victor.diec@me.com>
2016-03-26 23:40:04 -0400

    Updated the create tables script

--- Db/CreateTable.sql
+++ Db/CreateTable.sql
@@ -1,4 +1,4 @@
-set search_path="groupAssign";
+set search_path="GroupAssign";
 
 --Movies can have multiple roles
 --Roles(Characters) can belong to multiple movies
@@ -18,8 +18,17 @@ email VARCHAR(20),
 city VARCHAR(20),
 province VARCHAR(20),
 country VARCHAR(20)
-)
+);
 
+--?
+--Should we set up specific age ranges?
+CREATE TABLE AgeRange
+(
+AgeCategory VARCHAR(15) PRIMARY KEY,
+LowerLimit INTEGER UNIQUE,
+UpperLimit INTEGER UNIQUE,
+CHECK(UpperLimit > LowerLimit OR (LowerLimit=null AND UpperLimit<>NULL) OR (LowerLimit<>null AND UpperLimit=NULL))
+);
 
 --not sure about the device-used attribute and whether we really need it or not
 CREATE TABLE Profile
@@ -32,17 +41,9 @@ Occupation VARCHAR(20),
 FOREIGN KEY (UserID) REFERENCES Users,
 FOREIGN KEY (AgeCategory) REFERENCES AgeRange,
 CHECK (Gender IN('Male','Female'))
-)
+);
 
---?
---Should we set up specific age ranges?
-CREATE TABLE Agerange
-(
-AgeCategory VARCHAR(15) PRIMARY KEY,
-LowerLimit INTEGER UNIQUE,
-UpperLimit INTEGER UNIQUE,
-CHECK(UpperLimit > LowerLimit OR (LowerLimit=null AND UpperLimit<>NULL) OR (LowerLimit<>null AND UpperLimit=NULL))
-)
+
 
 CREATE TABLE Topics
 (
@@ -50,7 +51,7 @@ TopicID INTEGER PRIMARY KEY,
 Descirption TEXT
 --TEXT is supposed to be text of variable unlimited length
 --http://www.postgresql.org/docs/9.5/static/datatype-character.html
-)
+);
 
 CREATE TABLE Movie
 (
@@ -59,7 +60,7 @@ Name VARCHAR(20),
 Releasedate DATE,
 --the lower limit for what ages should watch this movie
 MPAARating INTEGER
-)
+);
 
 --relation between user and a movie
 CREATE TABLE Watches
@@ -70,9 +71,9 @@ WatchDate DATE,
 Rating INTEGER,
 PRIMARY KEY(UserID, MovieID),
 FOREIGN KEY(UserID) REFERENCES Users,
-FOREIGN KEY(MovieID) REFERENCES Movie
+FOREIGN KEY(MovieID) REFERENCES Movie,
 CHECK (RATING < 0 AND RATING < 6)
-)
+);
 
 --relation between movie and topic
 CREATE TABLE MovieTopics
@@ -86,7 +87,7 @@ PRIMARY KEY (TopicID, MovieID),
 FOREIGN KEY (TopicID) REFERENCES Topics,
 FOREIGN KEY (MovieID) REFERENCES Movie,
 CHECK (Subtitles IN('y','n'))
-)
+);
 
 CREATE TABLE Actor
 (
@@ -95,7 +96,7 @@ Lastname VARCHAR(20),
 Firstname VARCHAR(20),
 BirthDate DATE,
 Country VARCHAR(20)
-)
+);
 
 --The character that they play
 CREATE TABLE Role
@@ -104,7 +105,7 @@ RoleID VARCHAR(10) PRIMARY KEY UNIQUE,
 Name VARCHAR(20),
 ActorID VARCHAR(10),
 FOREIGN KEY (ActorID) REFERENCES Actor
-)
+);
 
 CREATE TABLE ActorStars
 (
@@ -113,7 +114,7 @@ ActorID VARCHAR(10),
 PRIMARY KEY(MovieID, ActorID),
 FOREIGN KEY (ActorID) REFERENCES Actor,
 FOREIGN KEY (MovieID) REFERENCES Movie
-)
+);
 
 CREATE TABLE Director
 (
@@ -121,23 +122,23 @@ DirectorID VARCHAR(10) PRIMARY KEY UNIQUE,
 Lastname VARCHAR(20),
 Firstname VARCHAR(20),
 Country VARCHAR(20)
-)
+);
 
 CREATE TABLE Directs
 (
 MovieID VARCHAR(20),
 DirectorID VARCHAR(10),
 PRIMARY KEY(MovieID, DirectorID),
-FOREIGN KEY (ActorID) REFERENCES Actor,
+FOREIGN KEY (MovieID) REFERENCES Movie,
 FOREIGN KEY (DirectorID) REFERENCES Director
-)
+);
 
 CREATE TABLE Studio
 (
 StudioID VARCHAR(10) PRIMARY KEY UNIQUE,
 Name VARCHAR(20),
 Country VARCHAR(20)
-)
+);
 
 CREATE TABLE Sponsors
 (
@@ -146,7 +147,7 @@ MovieID VARCHAR(20),
 PRIMARY KEY(MovieID, StudioID),
 FOREIGN KEY (StudioID) REFERENCES Studio,
 FOREIGN KEY (MovieID) REFERENCES Movie
-)
+);
 
 
 
