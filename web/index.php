033f391137dbed91d87858e6b2dd8894df8b6902
victordiec <victor.diec@me.com>
2016-03-31 21:56:13 -0400

    test see what happens

--- web/index.php
+++ web/index.php
@@ -1,54 +1,85 @@
-<?php
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
 
-//include 'core/init.php';
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-require('../vendor/autoload.php');
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
 
-$app = new Silex\Application();
-$app['debug'] = true;
- 
-$dbconn = pg_connect("host=web0.site.uottawa.ca port=15432 dbname=vraje059 user=vraje059 password=Vedha545654")
-    or die('Could not connect: ' . pg_last_error());
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 
-$query = "SELECT * FROM CUSTOMER";
-$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+    <?php
 
-// Printing results in HTML
-echo "<table>\n";
-while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-    echo "\t<tr>\n";
-    foreach ($line as $col_value) {
-        echo "\t\t<td>$col_value</td>\n";
+    //include 'core/init.php';
+
+    require('../vendor/autoload.php');
+
+    $app = new Silex\Application();
+    $app['debug'] = true;
+
+    $dbconn = pg_connect("host=web0.site.uottawa.ca port=15432 dbname=vraje059 user=vraje059 password=Vedha545654")
+        or die('Could not connect: ' . pg_last_error());
+
+    $query = "SELECT * FROM CUSTOMER";
+    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+    // Printing results in HTML
+    echo "<table>\n";
+    while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+        echo "\t<tr>\n";
+        foreach ($line as $col_value) {
+            echo "\t\t<td>$col_value</td>\n";
+        }
+        echo "\t</tr>\n";
     }
-    echo "\t</tr>\n";
-}
-echo "</table>\n";
+    echo "</table>\n";
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+    // Register the monolog logging service
+    $app->register(new Silex\Provider\MonologServiceProvider(), array(
+      'monolog.logfile' => 'php://stderr',
+    ));
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+    // Register view rendering
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/views',
+    ));
 
-// Our web handlers
+    // Our web handlers
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
+    $app->get('/', function() use($app) {
+      $app['monolog']->addDebug('logging output.');
+      return $app['twig']->render('index.twig');
+    });
 
-$app->get('/cowsay', function() use($app) {
-  $app['monolog']->addDebug('cowsay');
-  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
-});
+    $app->get('/cowsay', function() use($app) {
+      $app['monolog']->addDebug('cowsay');
+      return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+    });
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return str_repeat('Hello', getenv('TIMES'));
-});
+    $app->get('/', function() use($app) {
+      $app['monolog']->addDebug('logging output.');
+      return str_repeat('Hello', getenv('TIMES'));
+    });
 
-$app->run();
+    $app->run();
+    ?>
+
+
+  </body>
+</html>
