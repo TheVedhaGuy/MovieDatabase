b4d13b7d534f7cf255f6a978189d1be510837055
victordiec <victor.diec@me.com>
2016-04-02 03:15:09 -0400

    another test24

--- web/js/contact_me.js
+++ web/js/contact_me.js
@@ -14,41 +14,43 @@ $(function() {
             var username = $("input#username").val();
             var password = $("input#password").val();
 
-            $.ajax({
-                url: "././core/test.php",
-                type: "POST",
-                data: {
-                    username: username,
-                    password: password,
-                },
-                cache: false,
-                success: function(data) {
-                  console.log(data);
-                    // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+            console.log("js received " + username + " and " + password);
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function(data) {
-                  console.log(data);
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + username + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
+            // $.ajax({
+            //     url: "././core/test.php",
+            //     type: "POST",
+            //     data: {
+            //         username: username,
+            //         password: password,
+            //     },
+            //     cache: false,
+            //     success: function(data) {
+            //       console.log(data);
+            //         // Enable button & show success message
+            //         $("#btnSubmit").attr("disabled", false);
+            //         $('#success').html("<div class='alert alert-success'>");
+            //         $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            //             .append("</button>");
+            //         $('#success > .alert-success')
+            //             .append("<strong>Your message has been sent. </strong>");
+            //         $('#success > .alert-success')
+            //             .append('</div>');
+            //
+            //         //clear all fields
+            //         $('#contactForm').trigger("reset");
+            //     },
+            //     error: function(data) {
+            //       console.log(data);
+            //         // Fail message
+            //         $('#success').html("<div class='alert alert-danger'>");
+            //         $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            //             .append("</button>");
+            //         $('#success > .alert-danger').append("<strong>Sorry " + username + ", it seems that my mail server is not responding. Please try again later!");
+            //         $('#success > .alert-danger').append('</div>');
+            //         //clear all fields
+            //         $('#contactForm').trigger("reset");
+            //     },
+            // })
         },
         filter: function() {
             return $(this).is(":visible");
