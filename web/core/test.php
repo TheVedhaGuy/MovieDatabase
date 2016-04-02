ad94e26b744cf55452326a9c137d8016decb2600
victordiec <victor.diec@me.com>
2016-04-02 01:48:40 -0400

    another test4

--- web/core/test.php
+++ web/core/test.php
@@ -9,27 +9,27 @@ if(empty($_POST['username']) || empty($_POST['password']))
    else {
      echo "lol";
    }
-//
-// $username = $_POST['username'];
-// $password = $_POST['password'];
-//
-// $dbconn = pg_connect("host=web0.site.uottawa.ca port=15432 dbname=$username user=$username password=$password")
-//     or die('Could not connect: ' . pg_last_error());
-//
-// $query = "SELECT * FROM CUSTOMER";
-// $result = pg_query($query) or die('Query failed: ' . pg_last_error());
-//
-// // Printing results in HTML
-// echo "<table>\n";
-// while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-//     echo "\t<tr>\n";
-//     foreach ($line as $col_value) {
-//         echo "\t\t<td>$col_value</td>\n";
-//     }
-//     echo "\t</tr>\n";
-// }
-// echo "</table>\n";
-// echo "Hello World"
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$dbconn = pg_connect("host=web0.site.uottawa.ca port=15432 dbname=$username user=$username password=$password")
+    or die('Could not connect: ' . pg_last_error());
+
+$query = "SELECT * FROM CUSTOMER";
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+// Printing results in HTML
+echo "<table>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+echo "Hello World"
 
 return true
 
