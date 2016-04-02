4a2873c823569bc787f92db9868322d4342b106b
victordiec <victor.diec@me.com>
2016-04-02 02:13:57 -0400

    another test12

--- web/core/test.php
+++ web/core/test.php
@@ -24,17 +24,17 @@ $dbconn = pg_connect("host=web0.site.uottawa.ca port=15432 dbname=$username user
 $query = "SELECT * FROM CUSTOMER";
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
-// Printing results in HTML
-echo "<table>\n";
-while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-    echo "\t<tr>\n";
-    foreach ($line as $col_value) {
-        echo "\t\t<td>$col_value</td>\n";
-    }
-    echo "\t</tr>\n";
-}
-echo "</table>\n";
-echo "Hello World"
+// // Printing results in HTML
+// echo "<table>\n";
+// while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+//     echo "\t<tr>\n";
+//     foreach ($line as $col_value) {
+//         echo "\t\t<td>$col_value</td>\n";
+//     }
+//     echo "\t</tr>\n";
+// }
+// echo "</table>\n";
+// echo "Hello World"
 
 return true
 
